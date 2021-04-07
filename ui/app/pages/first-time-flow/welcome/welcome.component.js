f4a7c5965aa2e22d54639ebd5ba6cce34b029094
wk3368 <wk3368@163.com>
2021-04-07 10:26:12 +0800

    replace 3box with logo in home.html

--- ui/app/pages/first-time-flow/welcome/welcome.component.js
+++ ui/app/pages/first-time-flow/welcome/welcome.component.js
@@ -1,7 +1,5 @@
-import EventEmitter from 'events';
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-import Mascot from '../../../components/ui/mascot';
 import Button from '../../../components/ui/button';
 import {
   INITIALIZE_CREATE_PASSWORD_ROUTE,
@@ -18,12 +16,6 @@ export default class Welcome extends PureComponent {
   static contextTypes = {
     t: PropTypes.func,
   };
-
-  constructor(props) {
-    super(props);
-
-    this.animationEventEmitter = new EventEmitter();
-  }
 
   componentDidMount() {
     const { history, participateInMetaMetrics, welcomeScreenSeen } = this.props;
@@ -45,11 +37,7 @@ export default class Welcome extends PureComponent {
     return (
       <div className="welcome-page__wrapper">
         <div className="welcome-page">
-          <Mascot
-            animationEventEmitter={this.animationEventEmitter}
-            width="125"
-            height="125"
-          />
+          <img src="images/icon-128.png" width="125" height="125" alt="" />
           <div className="welcome-page__header">{t('welcome')}</div>
           <div className="welcome-page__description">
             <div>{t('metamaskDescription')}</div>
