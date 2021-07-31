90e2409186dd69523d6776f8f841c823aa9c6ac8
wk3368 <wk3368@163.com>
2021-08-01 00:13:29 +0800

    customGas page: replace ETH with STC

--- ui/app/helpers/utils/formatters.js
+++ ui/app/helpers/utils/formatters.js
@@ -1,4 +1,4 @@
 // TODO: Rename to reflect that this function is used for more cases than ETH, and update all uses.
-export function formatETHFee(ethFee, currencySymbol = 'ETH') {
+export function formatETHFee(ethFee, currencySymbol = 'STC') {
   return `${ethFee} ${currencySymbol}`;
 }
