21216813faca95168826c416308eaac2fa904d29
wk3368 <wk3368@163.com>
2021-11-19 10:55:23 +0800

    support creating multi sign account  in the create-account page

--- ui/app/pages/create-account/new-account.component.js
+++ ui/app/pages/create-account/new-account.component.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { utils, encoding } from '@starcoin/starcoin';
+import copyToClipboard from 'copy-to-clipboard';
+import ReadOnlyInput from '../../components/ui/readonly-input';
+import Dropdown from '../../components/ui/dropdown';
 import Button from '../../components/ui/button';
 
 export default class NewAccountCreateForm extends Component {
@@ -8,43 +12,174 @@ export default class NewAccountCreateForm extends Component {
   };
 
   state = {
+    number: 3,
+    threshold: 2,
+    localAccount: {},
+    externalPublicKeys: {},
+    type: this.context.t('normalAccount'),
     newAccountName: '',
     defaultAccountName: this.context.t('newAccountNumberName', [
+      '',
       this.props.newAccountNumber,
     ]),
   };
 
+  generatePublicKey() {
+    const privateKey = utils.account.generatePrivateKey();
+    encoding.privateKeyToPublicKey(privateKey).then((publicKey) => {
+      const localAccount = {
+        privateKey,
+        publicKey,
+      };
+      this.setState({ localAccount });
+    });
+  }
+
+  getMenuItemTexts() {
+    return [
+      this.context.t('normalAccount'),
+      this.context.t('multiSignAccount'),
+    ];
+  }
+
+  renderMultiSign() {
+    const { number, threshold } = this.state;
+    const contents = [];
+    contents.push(
+      <div key="0">
+        <div className="new-account-create-form__input-label">
+          {this.context.t('accountsNumber')}
+          <input
+            className="new-account-create-form__input new-account-create-form__input-threshold"
+            value={number}
+            placeholder="3"
+            onChange={(event) => this.setState({ number: event.target.value })}
+          />
+          &nbsp;&nbsp;&nbsp;&nbsp;{this.context.t('thresHold')}
+          <input
+            className="new-account-create-form__input new-account-create-form__input-threshold"
+            value={threshold}
+            placeholder="2"
+            onChange={(event) =>
+              this.setState({ threshold: event.target.value })
+            }
+          />
+        </div>
+      </div>,
+    );
+    contents.push(
+      <div key="1">
+        <div className="new-account-create-form__input-label">
+          {`${this.context.t('localTemp')}${this.context.t('publicKey')} 1 (${this.context.t('copyToClipboardOnClick')})`}
+        </div>
+        <div>
+          <ReadOnlyInput
+            textarea
+            wrapperClass="ellip-address-wrapper"
+            value={this.state.localAccount.publicKey}
+            onClick={() => copyToClipboard(this.state.localAccount.publicKey)}
+          />
+        </div>
+      </div>,
+    );
+    for (let i = 1; i < number; i++) {
+      const index = i + 1;
+      contents.push(
+        <div key={index}>
+          <div className="new-account-create-form__input-label">
+            {this.context.t('externalTemp')}{this.context.t('publicKey')} {index}
+          </div>
+          <div>
+            <textarea
+              rows="2"
+              className="new-account-create-form__input-publicKey"
+              value={this.state.externalPublicKeys[i] || ''}
+              placeholder=""
+              onChange={(event) => {
+                const externalPublicKeys = {
+                  ...this.state.externalPublicKeys,
+                  [i]: event.target.value,
+                };
+                this.setState({ externalPublicKeys });
+              }}
+            />
+          </div>
+        </div>,
+      );
+    }
+    return contents;
+  }
+
   render() {
-    const { newAccountName, defaultAccountName } = this.state;
+    const menuItems = this.getMenuItemTexts();
+    const { newAccountName, defaultAccountName, type, number, threshold, externalPublicKeys, localAccount } = this.state;
     const { history, createAccount, mostRecentOverviewPage } = this.props;
     const createClick = (_) => {
-      createAccount(newAccountName || defaultAccountName)
-        .then(() => {
-          this.context.metricsEvent({
-            eventOpts: {
-              category: 'Accounts',
-              action: 'Add New Account',
-              name: 'Added New Account',
-            },
+      if (type === this.context.t('normalAccount')) {
+        createAccount(newAccountName || defaultAccountName)
+          .then(() => {
+            this.context.metricsEvent({
+              eventOpts: {
+                category: 'Accounts',
+                action: 'Add New Account',
+                name: 'Added New Account',
+              },
+            });
+            history.push(mostRecentOverviewPage);
+          })
+          .catch((e) => {
+            this.context.metricsEvent({
+              eventOpts: {
+                category: 'Accounts',
+                action: 'Add New Account',
+                name: 'Error',
+              },
+              customVariables: {
+                errorMessage: e.message,
+              },
+            });
           });
-          history.push(mostRecentOverviewPage);
-        })
-        .catch((e) => {
-          this.context.metricsEvent({
-            eventOpts: {
-              category: 'Accounts',
-              action: 'Add New Account',
-              name: 'Error',
-            },
-            customVariables: {
-              errorMessage: e.message,
-            },
+      } else {
+        const publicKeys = Object.values(externalPublicKeys);
+        const privateKeys = [localAccount.privateKey];
+
+        utils.multiSign
+          .createMultiEd25519KeyShard(publicKeys, privateKeys, this.state.threshold)
+          .then((shard) => {
+            const multiAccount = utils.account.showMultiEd25519Account(shard);
+            history.push(mostRecentOverviewPage);
           });
-        });
+      }
     };
 
     return (
       <div className="new-account-create-form">
+        <div className="new-account-create-form__select-section">
+          <div className="new-account-import-form__select-label">
+            {this.context.t('selectType')}
+          </div>
+          <Dropdown
+            className="new-account-import-form__select"
+            options={menuItems.map((text) => ({ value: text }))}
+            selectedOption={type || menuItems[0]}
+            onChange={(value) => {
+              const _defaultAccountName = this.context.t(
+                'newAccountNumberName',
+                [
+                  value === menuItems[0] ? '' : this.context.t('multiSign'),
+                  this.props.newAccountNumber,
+                ],
+              );
+              if (!this.state.localAccount.publicKey) {
+                this.generatePublicKey();
+              }
+              this.setState({
+                type: value,
+                defaultAccountName: _defaultAccountName,
+              });
+            }}
+          />
+        </div>
         <div className="new-account-create-form__input-label">
           {this.context.t('accountName')}
         </div>
@@ -58,6 +193,7 @@ export default class NewAccountCreateForm extends Component {
             }
             autoFocus
           />
+          {type === menuItems[1] ? this.renderMultiSign() : null}
           <div className="new-account-create-form__buttons">
             <Button
               type="default"
