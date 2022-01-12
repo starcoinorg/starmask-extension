07b35053c89919189a50eb5584227b025659f3b6
wk3368 <wk3368@163.com>
2022-01-12 23:58:26 +0800

    call background.addTokens if an account's asset list has some tokens that are not added and accepted yet

--- ui/app/components/app/token-list/token-list.js
+++ ui/app/components/app/token-list/token-list.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { isEqual } from 'lodash';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import TokenCell from '../token-cell';
 import { useI18nContext } from '../../../hooks/useI18nContext';
 import { useTokenTracker, getTokenInfos } from '../../../hooks/useTokenTracker';
@@ -11,21 +11,22 @@ import {
   getSelectedAddress,
   getAssets,
 } from '../../../selectors';
-import { getTokens } from '../../../ducks/metamask/metamask';
+import { getTokens, getHiddenTokens } from '../../../ducks/metamask/metamask';
+import { addTokens } from '../../../store/actions';
 
 export default function TokenList({ onTokenClick }) {
+  const dispatch = useDispatch();
   const t = useI18nContext();
   const assetImages = useSelector(getAssetImages);
   const shouldHideZeroBalanceTokens = useSelector(
     getShouldHideZeroBalanceTokens,
   );
-
+  const hiddenTokens = useSelector(getHiddenTokens);
   // use `isEqual` comparison function because the token array is serialized
   // from the background so it has a new reference with each background update,
   // even if the tokens haven't changed
   const tokens = useSelector(getTokens, isEqual);
 
-  const [allTokens, setAllTokens] = useState([]);
   const [loadingTokenInfos, setLoadingTokenInfos] = useState(false);
   const userAddress = useSelector(getSelectedAddress);
   const assets = useSelector(getAssets, isEqual);
@@ -36,17 +37,22 @@ export default function TokenList({ onTokenClick }) {
         if (!loadingTokenInfos) {
           setLoadingTokenInfos(true);
           const assetTokens = await getTokenInfos(currentAssets);
+          const tokensCode = tokens.map((token) => token.code.toLowerCase())
+          const addedTokens = assetTokens.filter((token) =>
+            !(tokensCode.includes(token.code.toLowerCase()) || hiddenTokens.includes(token.code.toLowerCase()))
+          )
+          if (addedTokens.length) {
+            dispatch(addTokens(addedTokens));
+          }
           setLoadingTokenInfos(false);
-          const allTokens = tokens.concat(assetTokens)
-          setAllTokens(allTokens);
         }
       }
     };
     fetchData();
-  }, [userAddress, assets]);
+  }, [userAddress, assets, tokens, hiddenTokens]);
 
   const { loading, tokensWithBalances } = useTokenTracker(
-    allTokens,
+    tokens,
     true,
     shouldHideZeroBalanceTokens,
   );
