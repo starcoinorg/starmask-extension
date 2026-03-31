ba6b3d7c92cb73cd88989759663586d2bb636e29
KAGURA <2206176062@qq.com>
2026-03-31 18:08:58 +0800

    revert: undo node upgrade related changes (4 commits)

--- development/build/sass-compiler.js
+++ development/build/sass-compiler.js
@@ -1,13 +1,10 @@
 const sass = require('sass');
 
-const bridgeJson = (target) => JSON.parse(JSON.stringify(target))
-const bridgeFn = (target) => ((...args) => target(...args))
-
 module.exports = {
   render: (opts, callback) => {
     // sass wants its arguments to come from the same Realm as itself
     // bridgeJson and bridgeFn are added via patch-package to make this possible
-    sass.render(bridgeJson(opts), bridgeFn(callback));
+    sass.render(sass.bridgeJson(opts), sass.bridgeFn(callback));
   },
   renderSync: () => {
     throw new Error('sass-wrapper - renderSync not supported');
